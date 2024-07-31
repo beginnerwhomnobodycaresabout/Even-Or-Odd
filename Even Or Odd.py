64601544db1ce5f85c2c44522ebda951ed12c158
beginnerwhomnobodycaresabout <Delphalink2@gmail.com>
2024-07-31 13:11:40 +0500

    Add files via upload

---
+++ Even Or Odd.py
@@ -0,0 +1,5 @@
+number = int(input("Enter A Number To Check Wheather It Is Even Or Odd: "))
+if(number % 2 == 0):
+    print("Your Number Is An Even Number. ✨")
+else:
+    print("Your Number Is An odd Number. ✨")
